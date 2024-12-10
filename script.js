442d814d9f986eb18199891f4f014475e593aeae
viblo29 <dachivibliani29@gmail.com>
2024-12-10 14:31:20 +0400

    3rd

--- script.js
+++ script.js
@@ -14,41 +14,52 @@ randomPromise
 function RandomDelayPromise(taskname) {
   return new Promise((resolve) => {
     let delay = Math.floor(Math.random() * 2000) + 1000;
-    let seconds = delay / 1000;
+    let seconds = Math.floor(delay / 1000);
     setTimeout(() => {
       resolve(`${taskname} Resolved after ${seconds} seconds`);
     }, delay);
   });
 }
 
-Promise.all([RandomDelayPromise(`Task 2, 1st Promise:`), RandomDelayPromise(`Task 2, 2nd Promise:`), RandomDelayPromise(`Task 2, 3rd Promise:`)])
+Promise.all([
+  RandomDelayPromise(`Task 2, 1st Promise:`),
+  RandomDelayPromise(`Task 2, 2nd Promise:`),
+  RandomDelayPromise(`Task 2, 3rd Promise:`),
+])
   .then((results) => {
     console.log(results);
   })
   .catch((error) => console.log(error));
 
-Promise.race([RandomDelayPromise(`Task 3, 1st Promise:`), RandomDelayPromise(`Task 3, 2nd Promise:`)])
+Promise.race([
+  RandomDelayPromise(`Task 3, 1st Promise:`),
+  RandomDelayPromise(`Task 3, 2nd Promise:`),
+])
   .then((result) => {
     console.log(result);
   })
   .catch((error) => console.log(error));
 
-
-
-function randomTask4(taskname) {
-    return new Promise((resolve,reject) => {
-      let delay = Math.floor(Math.random() * 2000) + 1000;
-      let outcome = Math.random() < 0.5;
+function task4(taskname) {
+  return new Promise((resolve, reject) => {
+    let delay = Math.floor(Math.random() * 3000) + 1000;
+    let outcome = Math.random() < 0.5;
+    let seconds = Math.floor(delay / 1000);
+    setTimeout(() => {
       if (outcome) {
-        resolve(`Resolved`);
+        resolve(`${taskname} Resolved after ${seconds} seconds`);
       } else {
-        reject(`Rejected`);
+        reject(`${taskname} Rejected after ${seconds} seconds`);
       }
-    });
-  }
+    }, delay);
+  });
+}
 
-Promise.all([randomTask4(`Task 4, 1st Promise:`),randomTask4(),randomTask4(),randomTask4()])
-.then((results) => {
+Promise.allSettled([task4(`Task 4, 1st Promise`), task4(`Task 4, 2nd Promise`), task4(`Task 4, 3rd Promise`), task4(`Task 4, 4th Promise`), task4(`Task 4, 5th Promise`)])
+    .then(
+    (results) => {
     console.log(results);
-  })
-  .catch((error) => console.log(error));
+  }
+)
+.catch((error) => console.log(error));
+
