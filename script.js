129318478ad350be23dbf87dc35259effb7664f5
viblo29 <dachivibliani29@gmail.com>
2024-12-10 14:07:05 +0400

    2nd

--- script.js
+++ script.js
@@ -11,32 +11,32 @@ randomPromise
   .then((result) => console.log(result))
   .catch((error) => console.log(error));
 
-function RandomDelayPromise() {
+function RandomDelayPromise(taskname) {
   return new Promise((resolve) => {
     let delay = Math.floor(Math.random() * 2000) + 1000;
     let seconds = delay / 1000;
     setTimeout(() => {
-      resolve(`Resolved after ${seconds} seconds`);
+      resolve(`${taskname} Resolved after ${seconds} seconds`);
     }, delay);
   });
 }
 
-Promise.all([RandomDelayPromise(), RandomDelayPromise(), RandomDelayPromise()])
+Promise.all([RandomDelayPromise(`Task 2, 1st Promise:`), RandomDelayPromise(`Task 2, 2nd Promise:`), RandomDelayPromise(`Task 2, 3rd Promise:`)])
   .then((results) => {
-    console.log(`Task 2: ${results}`);
+    console.log(results);
   })
-  .catch((error) => console.error(error));
+  .catch((error) => console.log(error));
 
-Promise.race([RandomDelayPromise(), RandomDelayPromise()])
+Promise.race([RandomDelayPromise(`Task 3, 1st Promise:`), RandomDelayPromise(`Task 3, 2nd Promise:`)])
   .then((result) => {
-    console.log(`Task 3 ${result}`);
+    console.log(result);
   })
   .catch((error) => console.log(error));
 
 
 
-function randomTask4() {
-    return new Promise((resolve) => {
+function randomTask4(taskname) {
+    return new Promise((resolve,reject) => {
       let delay = Math.floor(Math.random() * 2000) + 1000;
       let outcome = Math.random() < 0.5;
       if (outcome) {
@@ -47,3 +47,8 @@ function randomTask4() {
     });
   }
 
+Promise.all([randomTask4(`Task 4, 1st Promise:`),randomTask4(),randomTask4(),randomTask4()])
+.then((results) => {
+    console.log(results);
+  })
+  .catch((error) => console.log(error));
